1f0926fbca8a4f6e67938a0ff18148379fff4b14
mishaf1203 <sduhHDFGH345>
2022-06-17 17:52:29 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,19 @@ public class Main {
           System.out.print(" "+i);
         }
         System.out.println("  ");
+
         for (i = 10; i > 0; i--){
             System.out.print(" "+i);
         }
+        System.out.println(" ");
+        int month = 31;
+        int friday = 3;
+        while (friday <  month){
+            friday = friday + 7;
+            System.out.println(friday);
+
+            }
+
     }
 
 }
