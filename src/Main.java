9e30b734942d20d0a8371511581ab5ce4457203b
mishaf1203 <sduhHDFGH345>
2022-06-17 03:58:56 +0300

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 10) {
+            i = i +1;
+          System.out.print(" "+i);
+        }
+        System.out.println("  ");
+        for (i = 10; i > 0; i--){
+            System.out.print(" "+i);
+        }
+    }
+
+}
